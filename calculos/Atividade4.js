9ae52e89287c2af13f0f7771a5c92f62b8458eb5
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-09-09 19:27:35 -0300

    Delete calculos directory

--- calculos/Atividade4.js
+++
@@ -1,9 +0,0 @@
-function Inss(){ 
-    
-    let salario = Number(document.getElementById('imposto').value);
-    let tributo = Number((salario/100)*14);
-    let result = Number(salario-tributo);
-
-    let tela = document.getElementById('printTela')
-    tela.innerHTML = "O salario é:"+salario+"<br>   O salario final é: "+result;
-}
