cae6e3d01d81a0178ea4164909585eafc32db638
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-09-10 16:09:29 -0300

    Add files via upload

---
+++ cenario_render/script.js
@@ -0,0 +1,68 @@
+(function () {
+    // selecionando o canvas e o contexto de renderização
+    var cnv = document.querySelector('canvas');
+    var ctx = cnv.getContext('2d');
+
+    // variavel quantidade de pixels
+    var pixel = 32;
+
+    // array que armazena as linhas e colunas do nosso cenario 2D 
+    var cenario = [
+
+        // adicionando 20 linhas nas 20 colunas do cenario
+        [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1]
+        
+    ];
+
+    function update() {
+        
+    }
+
+    function render() {
+        // varrendo os itens das linhas
+        for (const linha in cenario) {
+
+            //varrendo os elemnetos das colunas conforme as linhas
+            for (const coluna in cenario[linha]) {
+
+                // armazenando esse elemento
+                var elemento = cenario[linha][coluna];
+                
+                // verificando se o elemento for = 1 desenha um retangulo na tela
+                if (elemento === 1) {
+                    var x = coluna * pixel;
+                    var y = linha * pixel;
+
+                    // desenhando um retangulo
+                    ctx.fillRect(x,y,pixel,pixel);
+                }
+            }
+        }
+    }
+
+    function loop() {
+        update();
+        render();
+        requestAnimationFrame(loop, cnv);
+    }
+    requestAnimationFrame(loop, cnv);
+}());
